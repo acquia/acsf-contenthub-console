69de23b2aa5898e37e22b9046e54b9ea6fb835b2
Ateee89 <attila.tilman@cheppers.com>
2020-08-13 22:17:32 +0200

    LCH-4280: Checks for existing scheduled jobs.

---
+++ src/Command/AcsfCronCheck.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Acquia\Console\Acsf\Command;
+
+use Acquia\Console\Acsf\Platform\ACSFPlatform;
+use Acquia\Console\Cloud\Command\AcquiaCloudCronCheck;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Class AcsfCronCheck.
+ *
+ * @package Acquia\Console\Acsf\Command
+ */
+class AcsfCronCheck extends AcquiaCloudCronCheck {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $defaultName = 'acsf:cron:check';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this->setDescription('Checks for scheduled jobs which running ACH queues.');
+    $this->setAliases(['acsf-cch']);
+    $this->addOption('fix', 'f', InputOption::VALUE_NONE, 'Disable crons which running ACH queues.');
+  }
+
+  public static function getExpectedPlatformOptions(): array {
+    return ['source' => ACSFPlatform::getPlatformId()];
+  }
+
+  /**
+   * Get environment info from platform config.
+   *
+   * @return array
+   *   Environment config.
+   */
+  protected function getEnvironmentInfo(): array {
+    return [$this->platform->get('acquia.cloud.environment.name')];
+  }
+
+}
