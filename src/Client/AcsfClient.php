471acb4f2f055d1aaebf1198dcf07932d794ac5c
Narendra Desai <shenvidesainarendra@gmail.com>
2023-01-09 17:18:31 +0530

    LCH-6301: Avoid inheriting guzzle Client class.

--- src/Client/AcsfClient.php
+++ src/Client/AcsfClient.php
@@ -4,8 +4,11 @@ namespace Acquia\Console\Acsf\Client;
 
 use Acquia\Console\Acsf\Libs\Task\TaskException;
 use Acquia\Console\Acsf\Libs\Task\Tasks;
-use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\RequestOptions;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -13,9 +16,16 @@ use Psr\Log\LoggerInterface;
  *
  * @package Acquia\Console\Acsf\Client
  */
-class AcsfClient extends Client {
+class AcsfClient implements ClientInterface {
 
   use ResponseHandlerTrait;
+
+  /**
+   * GuzzleHttp client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
 
   /**
    * AcsfClient constructor.
@@ -40,7 +50,7 @@ class AcsfClient extends Client {
     ];
     $config = array_merge_recursive($default_conf, $config);
 
-    parent::__construct($config);
+    $this->httpClient = AcsfClientFactory::getGuzzleClient($config);
   }
 
   /**
@@ -171,7 +181,7 @@ class AcsfClient extends Client {
    */
   public function __call($method, $args) {
     try {
-      return parent::__call($method, $args);
+      return $this->httpClient->__call($method, $args);
     }
     catch (\Exception $e) {
       $this->logger->error($e->getMessage());
@@ -209,6 +219,41 @@ class AcsfClient extends Client {
       return new Tasks($tasks);
     }
     throw new TaskException('Tasks cannot be retrieved', TaskException::TASK_RETRIEVAL_ERROR);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function send(RequestInterface $request, array $options = []): ResponseInterface {
+    return $this->httpClient->send($request, $options);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function sendAsync(RequestInterface $request, array $options = []): PromiseInterface {
+    return $this->httpClient->sendAsync($request, $options);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function request($method, $uri, array $options = []): ResponseInterface {
+    return $this->httpClient->request($method, $uri, $options);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function requestAsync($method, $uri, array $options = []): PromiseInterface {
+    return $this->httpClient->requestAsync($method, $uri, $options);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig($option = NULL) {
+    return $this->httpClient->getConfig($option);
   }
 
 }
